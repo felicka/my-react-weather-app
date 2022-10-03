e3e724a45b6b4c99ad4eda6d5b05d3ce8b6bfe78
felicka <104525905+felicka@users.noreply.github.com>
2022-10-03 09:40:10 -0300

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-      React Weather App
+      <h1>React Weather App</h1>
+      <footer>
+        Weather App Coded by Felicia{" "}
+        <a
+          href="https://github.com/felicka/my-react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
