f54451f8556e5c40923a5e2c0aa5551e1fbfd13f
felicka <104525905+felicka@users.noreply.github.com>
2022-10-04 06:04:44 -0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <footer>
-        Weather App Coded by Felicia{" "}
-        <a
-          href="https://github.com/felicka/my-react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <Weather />
+        <footer>
+          Weather App Coded by Felicia{" "}
+          <a
+            href="https://github.com/felicka/my-react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            on GitHub
+          </a>
+          <button>Press</button>
+        </footer>
+      </div>
     </div>
   );
 }
