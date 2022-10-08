717ba2e3c83f38589e64b40091084e51bed34d28
felicka <104525905+felicka@users.noreply.github.com>
2022-10-08 12:50:57 -0300

    cleaned up code

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex">
-            <div classNme="float-left">
+            <div className="float-left">
               <WeatherIcon
                 code={props.data.icon}
                 size={50}
